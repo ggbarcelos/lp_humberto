77ab812b4f20707bcab09d3938b863350aeed406
Glauber Barcelos <glauberbarcelos@hotmail.com>
2026-07-07 10:35:01 -0300

    Revert "alteraçoes na sessao hero"
    This reverts commit dbac0fc569a970792e17b47568c8261d407e053e.

--- js/custom.js
+++ js/custom.js
@@ -4,12 +4,25 @@ AOS.init({
   offset: 50,
 });
 
+const navbar = document.querySelector('.navbar');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
     const target = document.querySelector(this.getAttribute('href'));
     if (target) {
       target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+    const navCollapse = document.querySelector('.navbar-collapse');
+    if (navCollapse.classList.contains('show')) {
+      navCollapse.classList.remove('show');
     }
   });
 });
